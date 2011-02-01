91d080b21a7c7a97fda8cf60e48cab87e3f4f7f4
Nowa <nowazhu@gmail.com>
2011-02-01 23:41:56 +0800

    Init by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huijia123::Application.config.secret_token = '9273077598845e6b470ee1611babe27005dc9577d0e90cafb2625fc8c66f8454ef30e98ec45bc5b53701da3beebc336c080399744c30c0945c55af10af10f457'
